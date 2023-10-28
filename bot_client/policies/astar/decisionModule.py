396ace4eadb77a28c2ce9e2cb7c852c58b8c421c
Ernest McCarter <ernestmccarter@Ernests-MacBook-Pro.local>
2023-10-28 18:07:38 -0400

    add small pellet chaser

--- bot_client/policies/astar/decisionModule.py
+++ bot_client/policies/astar/decisionModule.py
@@ -4,7 +4,6 @@ import asyncio
 # Other
 from enum import IntEnum
 import math
-import os
 from policies.astar.fieldnodes import FIELD_NODES
 from policies.astar.basicAStar import BasicAStar
 
@@ -14,9 +13,9 @@ from gameState import Directions, GameModes, GameState
 from websockets.sync.client import ClientConnection # type: ignore
 
 
-
+# Constants
 INFINITY = 99999999999
-
+SUPER_PELLET_LOCATIONS = [(1,23), (26,23), (1,3), (26,3)]
 COMMAND_TO_BYTES = {
 	Directions.RIGHT: b'd',
 	Directions.LEFT: b'a',
@@ -26,9 +25,8 @@ COMMAND_TO_BYTES = {
 
 class DecisionStates(IntEnum):
 	'''
-	Enum of possible game modes
+	Enum of different states in our state machine
 	'''
-
 	SUPER_PELLET_CHASER  	= 0
 	GHOST_CHASER		 	= 1
 	SMALL_PELLET_CHASER   	= 2
@@ -52,12 +50,15 @@ class DecisionModule:
 		self.debug = debug
 		
 		# Decision State
-		# 0 = Super Pellet Hunter
-		# 1 = Ghost Chaser
 		self.decision_state = DecisionStates.SUPER_PELLET_CHASER
 
-		# Game Info
-		self.game_super_pellet_locs = [(1,23), (26,23), (1,3), (26,3)]
+		# Make a list of super pellets on the field (this may vary if we restart our client in the middle of a round)
+		self.super_pellets = SUPER_PELLET_LOCATIONS
+		self.field_has_super_pellets = True
+		# for col, row in SUPER_PELLET_LOCATIONS:
+		# 	if self.state.superPelletAt(col=col, row=row):
+		# 		self.super_pellets.append((col,row))
+		# self._log(self.super_pellets)
 
 		# Last command
 		self.last_command = None
@@ -66,9 +67,9 @@ class DecisionModule:
 		self.field_nodes = FIELD_NODES
 
 
-	def _log(self, message: str, end='\n'):
+	def _log(self, message, end='\n'):
 		if self.debug:
-			print('Decision Module: ' + message, end=end)
+			print('Decision Module: ' + str(message), end=end)
 
 	def get_field_location(self, location):
 		'''
@@ -142,17 +143,17 @@ class DecisionModule:
 		'''
 		returns closest super pellet location
 		'''
-		if len(self.game_super_pellet_locs) == 0:
+		if len(self.super_pellets) == 0:
 			return None
 		closest_pellet = 0
 		smallest_dist = INFINITY
-		for i in range(len(self.game_super_pellet_locs)):
-			dist = (self.state.pacmanLoc.col - self.game_super_pellet_locs[i][0]) ** 2 \
-						+ (self.state.pacmanLoc.row - self.game_super_pellet_locs[i][0]) ** 2
+		for i in range(len(self.super_pellets)):
+			dist = (self.state.pacmanLoc.col - self.super_pellets[i][0]) ** 2 \
+						+ (self.state.pacmanLoc.row - self.super_pellets[i][0]) ** 2
 			if dist < smallest_dist:
 				smallest_dist = dist
 				closest_pellet = i
-		return '(' + str(self.game_super_pellet_locs[closest_pellet][0]) + ',' + str(self.game_super_pellet_locs[closest_pellet][1]) + ')'
+		return '(' + str(self.super_pellets[closest_pellet][0]) + ',' + str(self.super_pellets[closest_pellet][1]) + ')'
 	
 	def get_closest_ghost(self):
 		'''
@@ -186,7 +187,7 @@ class DecisionModule:
 		'''
 		Get the closest ghost that can be eaten (this does not include ghosts that are still spawning)
 		'''
-		closest_ghost = self.state.ghosts[0]
+		closest_ghost = None
 		smallest_dist = INFINITY
 		for ghost in self.state.ghosts:
 			dist = (self.state.pacmanLoc.col - ghost.location.col) ** 2 \
@@ -194,25 +195,74 @@ class DecisionModule:
 			if dist < smallest_dist and ghost.frightSteps != 0 and ghost.spawning == False:
 				smallest_dist = dist
 				closest_ghost = ghost
+		# if no ghosts, return closest pellet
+		if closest_ghost == None:
+			return self.game_get_closest_small_pellet()
+		# return closest ghost location
 		return '(' + str(closest_ghost.location.col) + ',' + str(closest_ghost.location.row) + ')'
+	
+	def game_get_closest_small_pellet(self):
+		queue = []
+		visited = []
+
+		# getting location of pacbot
+		curr = self.get_field_location(self.state.pacmanLoc)
+
+		# add to queue
+		queue.append(curr)
+
+		while queue:
+			# remove curr from the queue
+			curr = queue.pop(0)
+			
+			# check for the flag (does pellet exist)
+			col, row = eval(curr)
+			if self.state.pelletAt(col=col, row=row):
+				return curr
+			
+			# add edges to the queue
+			edges = self.field_nodes[curr]
+			for edge in edges:
+				if edge[0] not in visited:
+					queue.append(edge[0])
+	
+			# add curr to visited
+			visited.append(curr);	
 		
 
 	def decision_transition(self):
+		# update super pellets TODO: move this somewhere else so we don't have to recalc each time?
+		# for col, row in SUPER_PELLET_LOCATIONS:
+		# 	if self.state.superPelletAt(col=col, row=row):
+		# 		self.super_pellets.append((col,row))
 		# remove each pellet
-		for i, super_pellet in enumerate(self.game_super_pellet_locs):
-			if '(' + str(self.state.pacmanLoc.col) + ', ' + str(self.state.pacmanLoc.row) + ')' == str(super_pellet):
-				del self.game_super_pellet_locs[i]
+		for i, super_pellet in enumerate(self.super_pellets):
+			if '(' + str(self.state.pacmanLoc.col) + ', ' + str(self.state.pacmanLoc.row) + ')' == str(super_pellet): # TODO: Use eval
+				del self.super_pellets[i]
+
+		# transition from small pellet chaser
+		if self.decision_state == DecisionStates.SMALL_PELLET_CHASER:
+			return
 
 		# transition from super pellet hunter to ...
 		if self.decision_state == DecisionStates.SUPER_PELLET_CHASER:
-			# check if any ghosts are frightened
+			# check if all the super pellets are gone -> SMALL_PELLET_CHASER
+			# self.state.superPelletAt(...)
+
+			# check if 0 super pellets left -> SMALL_PELLET_CHASER
+			if len(self.super_pellets) == 0:
+				self.decision_state = DecisionStates.SMALL_PELLET_CHASER
+				return
+
+			# check if any ghosts are frightened -> GHOST_CHASER
 			for ghost in self.state.ghosts:
 				if ghost.frightSteps != 0 and ghost.spawning == False:
 					self.decision_state = DecisionStates.GHOST_CHASER
-					break
+					return
 
 		# transition from ghost hunter to ...
-		elif self.decision_state == DecisionStates.GHOST_CHASER:
+		if self.decision_state == DecisionStates.GHOST_CHASER:
+			# check if ghosts are no longer frightened -> SUPER_PELLET_CHASER
 			change_state = True
 			for ghost in self.state.ghosts:
 				if ghost.frightSteps != 0 and ghost.spawning == False:
@@ -226,9 +276,11 @@ class DecisionModule:
 		Get the target to astar to
 		'''
 		# make a decision based on current state
+		if self.decision_state == DecisionStates.SMALL_PELLET_CHASER:
+			return self.game_get_closest_small_pellet()
 		if self.decision_state == DecisionStates.SUPER_PELLET_CHASER:
 			return self.game_get_closest_super_pellet()
-		elif self.decision_state == DecisionStates.GHOST_CHASER:
+		if self.decision_state == DecisionStates.GHOST_CHASER:
 			return self.game_get_closest_edible_ghost()
 		return '(0,0)'
 	
@@ -281,11 +333,13 @@ class DecisionModule:
 
 		# make sure game is playing
 		if self.state.gameMode == GameModes.PAUSED:
+			self._log('Game Paused')
 			return None
 
 		# make sure pacman in field
-		if self.state.pacmanLoc.col < 0 or self.state.pacmanLoc.col >= 32 \
-			or self.state.pacmanLoc.row < 0 or self.state.pacmanLoc.row >= 28:
+		if self.state.pacmanLoc.col < 0 or self.state.pacmanLoc.col >= 28 or self.state.pacmanLoc.row < 0 or self.state.pacmanLoc.row >= 31:
+			self._log('Pacbot gone!')
+			self._log('COL: ' + str(self.state.pacmanLoc.col) + ', ROW: ' + str(self.state.pacmanLoc.row))
 			return None
 
 		# print('Decision Module: target - ' + str(target))
@@ -357,5 +411,5 @@ class DecisionModule:
 			# Free up the event loop (a good chance to talk to the bot!)
 			await asyncio.sleep(0.05) # 1000 previous
 
-			# (REMOVE THIS) Unlock the game state
+			# Unlock the game state
 			self.state.unlock()
